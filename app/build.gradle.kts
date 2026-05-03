fa148e4fa2b2e701d8c982c405d4baf1748a23d7
useripx <yogiario316@gmail.com>
2026-05-03 09:43:01 +0700

    Lower minSdk to 24 (Android 7.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.googlehome.protect"
-        minSdk = 29
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
